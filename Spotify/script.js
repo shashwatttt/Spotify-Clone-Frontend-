2e57fde6b00be54c765ebde03259db84a7bab3fa
SHASHWAT MAURYA <shashwatmaurya154@gmail.com>
2024-04-20 22:35:21 +0530

    Final Code
    Spotify Frontend Clone using HTML, CSS, JavaScript.

---
+++ Spotify/script.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.getElementById("languageModal").style.display = "block";
+  }
+
+  function closeModal() {
+    document.getElementById("languageModal").style.display = "none";
+  }
